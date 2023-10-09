de54200d1d86f63a921c7158e2d45af866827d37
HenriqueDelben <hd.delben@gmail.com>
2023-10-09 11:53:01 -0300

    :sparkles: feat: Add getters and setters

--- src/main/java/br/app/cashew/feature02/user/dto/user/input/UserEmailChangeDTO.java
+++ src/main/java/br/app/cashew/feature02/user/dto/user/input/UserEmailChangeDTO.java
@@ -1,8 +1,10 @@
 package br.app.cashew.feature02.user.dto.user.input;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserEmailChangeDTO {
 
     private String email;
--- src/main/java/br/app/cashew/feature02/user/dto/user/input/UserUpdateInfoDTO.java
+++ src/main/java/br/app/cashew/feature02/user/dto/user/input/UserUpdateInfoDTO.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserUpdateInfoDTO {
 
     @Email(message = "Email invalido")
