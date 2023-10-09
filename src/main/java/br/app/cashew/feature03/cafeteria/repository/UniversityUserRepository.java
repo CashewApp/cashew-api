26a76c3c22f442e61de8b597657188886a180ddd
HenriqueDelben <hd.delben@gmail.com>
2023-10-09 10:20:11 -0300

    :boom: fix: change argument type from "User" to int to match "UniversityUser"'s composite key userID's type
    Before fix: repository would throw an exception every time the method "findByUniversityUserIDUserID" was executed

--- src/main/java/br/app/cashew/feature03/cafeteria/repository/UniversityUserRepository.java
+++ src/main/java/br/app/cashew/feature03/cafeteria/repository/UniversityUserRepository.java
@@ -2,7 +2,6 @@ package br.app.cashew.feature03.cafeteria.repository;
 
 import br.app.cashew.feature03.cafeteria.model.universityuser.UniversityUser;
 import br.app.cashew.feature03.cafeteria.model.universityuser.UniversityUserKey;
-import br.app.cashew.feature01.authentication.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +10,5 @@ import java.util.List;
 @Repository
 public interface UniversityUserRepository extends JpaRepository<UniversityUser, UniversityUserKey> {
 
-    List<UniversityUser> findByUniversityUserIDUserID(User user);
+    List<UniversityUser> findByUniversityUserIDUserID(int userID);
 }
